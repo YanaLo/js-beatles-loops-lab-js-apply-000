1dfedb0809619866938e353a66bcb5014ffc5c0e
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-23 20:13:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(arrayMusic,arrayInstrum){
   let emptyArray=[]
   for(let i=0;i<=arrayMusic.length-1;i++){
     let another=arrayMusic[i]+" plays "+arrayInstrum[i];
-    arrayInstrum[i]++;
+    //arrayInstrum[i]++;
     emptyArray.push(another);
   }
   return emptyArray;
